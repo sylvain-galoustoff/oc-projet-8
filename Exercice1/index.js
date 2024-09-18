0f762602cb92b662d9b5b4f84f77e4384faf0fee
Sylvain-MED <sylvain.galoustoff@gmail.com>
2024-09-18 14:55:12 +0200

    Exercice 1 fait

--- Exercice1/index.js
+++ Exercice1/index.js
@@ -1,3 +1,15 @@
-//votre code ici
+const pairNumbers = (start, end) => {
+  const response = [];
+  const startNumber = Number(start);
+  const endNumber = Number(end);
+  for (let i = startNumber; i <= endNumber; i++) {
+    if (i % 2 === 0) {
+      response.push(i);
+    }
+  }
+  return response.join(",");
+};
 
-export default pairNumbers
+console.log(pairNumbers("22", "31"));
+
+export default pairNumbers;
